b18aea560f14f02625fe244fae9e35d48b452c4b
Lamplions123 <161035224+Lamplions123@users.noreply.github.com>
2026-06-28 10:57:38 +0400

    don't set cookies if request fails (#7)

--- assets/js/github.js
+++ assets/js/github.js
@@ -5,12 +5,14 @@ async function get_release_version(owner, repo, getOnlyTag = false) {
     if (get_cookie_value(`${owner}_${repo}_tag`) === undefined
         || get_cookie_value(`${owner}_${repo}_ts`) === undefined) {
         const res = await fetch(`https://api.github.com/repos/${owner}/${repo}/releases/latest`);
+        
+        if (res.status === 200) {
+            const data = await res.json();
+            const timestamp = Date.parse(data.published_at);
 
-        const data = await res.json();
-        const timestamp = Date.parse(data.published_at);
-
-        document.cookie = `${owner}_${repo}_tag=${data.tag_name};max-age=86400`;
-        document.cookie = `${owner}_${repo}_ts=${timestamp};max-age=86400`;
+            document.cookie = `${owner}_${repo}_tag=${data.tag_name};max-age=86400`;
+            document.cookie = `${owner}_${repo}_ts=${timestamp};max-age=86400`;
+        }
     }
 
     if (getOnlyTag) {
